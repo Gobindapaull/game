37e7b36ffb9750ad008afc329c24f6fbca1777ed
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2021-12-06 19:52:38 +0530

    Add files via upload

---
+++ game2.js
@@ -0,0 +1,61 @@
+ //    text disappear after 5s
+
+setTimeout(function aa() {
+    document.getElementById("box1").style.display = "none";
+}, 5000 );
+
+setTimeout(function bb() {
+    document.getElementById("box2").style.display = "none";
+}, 5000 );
+setTimeout(function cc() {
+    document.getElementById("box3").style.display = "none";
+}, 5000 );
+setTimeout(function dd() {
+    document.getElementById("box4").style.display = "none";
+}, 5000 );
+setTimeout(function ee() {
+    document.getElementById("box5").style.display = "none";
+}, 5000 );
+setTimeout(function ff() {
+    document.getElementById("box6").style.display = "none";
+}, 5000 );
+setTimeout(function gg() {
+    document.getElementById("box7").style.display = "none";
+}, 5000 );
+setTimeout(function hh() {
+    document.getElementById("box8").style.display = "none";
+}, 5000 );
+
+
+       // box disappear after 4s
+       setTimeout(function ii() {
+        document.getElementById("div1").style.display = "none";
+    }, 7000 );  
+    setTimeout(function jj() {
+        document.getElementById("div2").style.display = "none";
+    }, 7000 );  
+    setTimeout(function kk() {
+        document.getElementById("div3").style.display = "none";
+    }, 7000 ); 
+    setTimeout(function ll() {
+        document.getElementById("div4").style.display = "none";
+    }, 7000 );  
+    setTimeout(function mm() {
+        document.getElementById("div5").style.display = "none";
+    }, 7000 );  
+    setTimeout(function nn() {
+        document.getElementById("div6").style.display = "none";
+    }, 7000 );  
+    setTimeout(function oo() {
+        document.getElementById("div7").style.display = "none";
+    }, 7000 );   
+    setTimeout(function pp() {
+        document.getElementById("div8").style.display = "none";
+    }, 7000 );  
+
+
+    // setTimeout(function final(text,fnt, colour , ) {
+    //           var x = "failed";
+    //           var y = "passed";
+    //           document.write(x);
+    // },8000 )
